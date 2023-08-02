3c19137bff2b4c9bb27f179ba124dadeb8e38590
SoyAgustin <Agustinphysx@gmail.com>
2023-08-02 13:45:44 -0600

    obteniendo los elementos de la lista con un for each y usando generics en la lista

--- mx/com/bytebank/test/TestArrayList.java
+++ mx/com/bytebank/test/TestArrayList.java
@@ -6,22 +6,27 @@ import mx.com.bytebank.modelo.*;
 
 public class TestArrayList {
 	public static void main(String[] args) {
-		ArrayList lista = new ArrayList();
+		ArrayList<Cuenta> lista = new ArrayList<>();
 		Cuenta cc = new CuentaCorriente(11, 2);
 		Cuenta cc2 = new CuentaCorriente(12, 3);
 		
 		lista.add(cc);
 		lista.add(cc2);
 		
-		Cuenta obtenerCuenta = (Cuenta) lista.get(0);
+		//Se puede eliminar el cast porque ya sabe que todos los elementos de la lista son de tipo Cuenta.
+		Cuenta obtenerCuenta =  lista.get(0);
 		//System.out.println(obtenerCuenta);
 		
 		//¿como podria obtener todos los elementos de la lista
 		
-		for (int i =0;i<lista.size();i++) {
+		//Usando un for each
+		int i = 0;
+		for(Cuenta cuenta : lista) {
 			System.out.println("-----elemento "+i+"-------");
-			System.out.println(lista.get(i));
+			System.out.println(cuenta);
 			System.out.println("----------------------\n");
-		}
+			i++;
+		}		
+		
 	}
 }
