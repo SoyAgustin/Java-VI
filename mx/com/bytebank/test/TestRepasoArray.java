e86297e622a25c93a5f521562a12a9b0001562a6
Agustin <agustinphysx@gmail.com>
2023-08-08 15:26:24 -0600

    Revision del wrapper Integer en TestRepasoArray.java

--- mx/com/bytebank/test/TestRepasoArray.java
+++ mx/com/bytebank/test/TestRepasoArray.java
@@ -1,29 +1,47 @@
 package mx.com.bytebank.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestRepasoArray {
 	public static void main(String[] args) {
 		
-		/*
-		 * Ya sabemos que un entero 'int' es un primitivo
-		 * y es diferente de un Objeto.
-		 * Existe la clase Integer como lo sería String.
-		 * Entonces los metodos como ArrayList que comen 
-		 * objetos pueden guardar automaticamente (autoboxing)
-		 * los enteros.
-		 * */
+		/**
+		 ** Ya sabemos que un entero 'int' es un primitivo
+		 ** y es diferente de un Objeto.
+		 ** Existe la clase Integer como lo sería String.
+		 ** Entonces los metodos como ArrayList que comen 
+		 ** objetos pueden guardar automaticamente (autoboxing)
+		 ** los enteros.
+		 ** **/
 		
 		int numero=40;
-		Integer numeroObjeto = new Integer(40);
+		//Esa forma de 'envolver' se llma WRAPPER.
 		
-		List<Integer> lista = new ArrayList<Integer>();
+		/*autoboxing: cuando un metodo necesita un objeto
+		 * y se detecta un primitivo automaticamente hace 
+		 * el siguiente método por debajo*/
+		Integer numeroObjeto = Integer.valueOf(40);
 		
-		lista.add(numero);
-		lista.add(numeroObjeto);
-		lista.add(new Integer(40)); // Esto es lo que hace por debajo (autoboxing)
 		
+		/*unboxing: Lo contrario a autoboxing, si lo que se 
+		 *necesita es un entero a partir del objeto
+		 *java lo extrae con el siguiente método*/
+		
+		int valorPimitivo = numeroObjeto.intValue();
+		//Equivalentemente se puede hacer lo siguiente 
+		//de forma mas comoda 
+		int valorPrimitivoCorto = numeroObjeto;
+		
+		/*Con los Wrappers podemos obtener el valor en otros
+		 * tipos de valores, por ejemplo*/
+		byte byteInteger = numeroObjeto.byteValue();
+		double doubleInteger = numeroObjeto.floatValue();
+		
+		/*
+		 * Tambien se pueden obtener los valores máximos y mínimos de los tipos de datos*/
+		
+		System.out.println(Integer.MAX_VALUE); //el numero mas grande soportado
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.SIZE);//el numero pero en bits
+		System.out.println(Integer.BYTES);
 		
 	}
 }
